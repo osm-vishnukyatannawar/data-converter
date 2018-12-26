72b43e2fc3734145731327d3ec1f3edebda05004
Vishnu Kyatannawar <vishnu.k@osmosys.asia>
2018-12-27 00:45:06 +0530

    Update the script

--- marketshare-converter.js
+++ marketshare-converter.js
@@ -43,40 +43,44 @@ async function start() {
       const currReadFile = path.join(currSourceDirectoryPath, files[j]);
       const currWriteFile = path.join(currDestinationDirectoryPath, files[j]);
       const currMap = mapping[webstiefolders[i]];
-      await (async function () {
-        let writeStream = fs.createWriteStream(currWriteFile);
-        const rl = readline.createInterface({
-          input: fs.createReadStream(currReadFile),
-          crlfDelay: Infinity
-        });
-
-        rl.on('line', (line) => {
-          const extractionData = JSON.parse(line).result.extractorData;
-          if (extractionData.hasOwnProperty('data')) {
-            const jsonData = extractionData.data[0].group[0];
-            let normalizedData = {};
-            normalizedData.source = currMap.source;
-            for (let prop in jsonData) {
-              if (!currMap.hasOwnProperty(prop)) {
-                continue;
-              }
-              normalizedData[currMap[prop]] = jsonData[prop][0].text;
-            }
-            writeStream.write(`${JSON.stringify(normalizedData)}\n`);
-          }
-        });
-
-        rl.on('close', () => {
-          writeStream.end();
-          return true;
-        });
-        // the finish event is emitted when all data has been flushed from the stream
-        writeStream.on('finish', () => {
-          console.log('wrote all data to file');
-        });
-      }());
+      console.log('\nStart');
+      console.log(`Normalizing ${files[j]}`);
+      await normalization(currWriteFile, currReadFile, currMap);
+      console.log('END');
     }
   }
 }
+
+async function normalization(currWriteFile, currReadFile, currMap) {
+  return new Promise((resolve, reject) => {
+    let writeStream = fs.createWriteStream(currWriteFile);
+    const rl = readline.createInterface({
+      input: fs.createReadStream(currReadFile),
+      crlfDelay: Infinity
+    });
+
+    rl.on('line', (line) => {
+      const extractionData = JSON.parse(line).result.extractorData;
+      if (extractionData.hasOwnProperty('data')) {
+        const jsonData = extractionData.data[0].group[0];
+        let normalizedData = {};
+        normalizedData.source = currMap.source;
+        for (let prop in jsonData) {
+          if (!currMap.hasOwnProperty(prop)) {
+            continue;
+          }
+          normalizedData[currMap[prop]] = jsonData[prop][0].text;
+        }
+        writeStream.write(`${JSON.stringify(normalizedData)}\n`);
+      }
+    });
+
+    rl.on('close', () => {
+      writeStream.end();
+      resolve(true);
+    });
+  });
+}
+
 
 start();
